89a67a3a8a3bf58c9f58086f4d1300669219a513
dk <dk@425babaa-d5dd-46a5-afc5-8c713db7da9f>
2013-08-03 11:42:26 +0000

    ライブラリ構成変更
    git-svn-id: file:///opt/svn/repository/projects/trunk@185 425babaa-d5dd-46a5-afc5-8c713db7da9f

---
+++ test/jp/co/dk/browzer/TestAll.java
@@ -0,0 +1,17 @@
+package jp.co.dk.browzer;
+
+import jp.co.dk.browzer.html.element.TestA;
+import jp.co.dk.browzer.html.element.TestForm;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ 
+	TestBrowser.class,
+	TestPage.class,
+	TestA.class,
+	TestForm.class,
+})
+public class TestAll {}
