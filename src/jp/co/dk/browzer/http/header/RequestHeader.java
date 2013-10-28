b4e3c337f32c0a2ed132f71620fc96e7fe10f023
dk <dk@425babaa-d5dd-46a5-afc5-8c713db7da9f>
2013-10-28 16:40:13 +0000

    クローラ作成中 Headerクラスの実装を変更
    git-svn-id: file:///opt/svn/repository/projects/trunk@329 425babaa-d5dd-46a5-afc5-8c713db7da9f

---
+++ src/jp/co/dk/browzer/http/header/RequestHeader.java
@@ -0,0 +1,33 @@
+package jp.co.dk.browzer.http.header;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import jp.co.dk.browzer.exception.BrowzingException;
+
+/**
+ * RequestHeaderは、HTTP通信にて使用されるHTTPヘッダを表すクラス
+ * 
+ * @version 1.0
+ * @author D.Kanno
+ */
+public class RequestHeader implements Serializable{
+
+	/** シリアルバージョンID */
+	private static final long serialVersionUID = 5547244168178351392L;
+	
+	private Map<String, String> headerMap;
+	
+	/**
+	 * コンストラクタ<br/>
+	 * 
+	 * マップを元にその通信にて使用されるHTTPヘッダを取り出し本オブジェクトに保持します。
+	 * 
+	 * @param responseHeader レスポンスヘッダー
+	 * @throws BrowzingException ヘッダ設定値が不正、またはサポートできない場合
+	 */
+	public RequestHeader(Map<String, String> requestHeader) throws BrowzingException {
+		this.headerMap = new HashMap<String, String>(requestHeader);
+	}
+}
