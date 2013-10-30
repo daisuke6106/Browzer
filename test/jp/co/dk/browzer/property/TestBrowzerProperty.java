a52920ce62c9c7a9fd4ea7f98bc5b77a25e3d2e8
dk <dk@425babaa-d5dd-46a5-afc5-8c713db7da9f>
2013-10-30 16:57:10 +0000

    クローラ作成中 Broezerプロパティの実装を変更
    git-svn-id: file:///opt/svn/repository/projects/trunk@332 425babaa-d5dd-46a5-afc5-8c713db7da9f

---
+++ test/jp/co/dk/browzer/property/TestBrowzerProperty.java
@@ -0,0 +1,21 @@
+package jp.co.dk.browzer.property;
+
+import static org.junit.Assert.*;
+import jp.co.dk.property.Property;
+
+import org.junit.Test;
+
+public class TestBrowzerProperty {
+
+	@Test
+	public void test() {
+		java.util.List<Property> requestHeader = BrowzerProperty.REQUEST_HEADER.getPropertyList();
+		for (Property property : requestHeader) {
+			String[] list = property.getStringArray();
+			StringBuilder sb = new StringBuilder();
+			for (String value : list)sb.append(value).append(',');
+			System.out.println(property.getKey() + "=" + sb.toString().substring(0, sb.length()-1));
+		}
+	}
+
+}
